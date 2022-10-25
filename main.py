03d7979732fc8ed5320bbc25cc7c20ae1d3cad2d
pchchv <ipchchv@gmail.com>
2022-10-25 21:05:20 +0300

    fix

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ Running tests and publishing to PyPI
 from os.path import join
 
 
-with open(join('icecream', '__version__.py'), encoding="utf-8") as f:
+with open(join('pychecker', '__version__.py'), encoding="utf-8") as f:
     meta = f.read()
