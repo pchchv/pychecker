85be4cd6293d7fe9dce62cf92d565db779fa2d9a
pchchv <ipchchv@gmail.com>
2022-10-26 13:10:34 +0300

    Implement test running

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ Running tests and publishing to PyPI
 
 
 import os
-import sys
-from os.path import join
+from pychecker import *
+from os.path import dirname, join
+from setuptools.command.test import test
 from setuptools import Command
 
 
@@ -25,7 +26,21 @@ class Publish(Command):
 
     def run(self):
         os.system('python setup.py sdist bdist_wheel')
-        sdist = 'dist/pychecker-%s.tar.gz' % meta['__version__']
-        wheel = 'dist/pychecker-%s-py2.py3-none-any.whl' % meta['__version__']
-        rc = os.system('twine upload "%s" "%s"' % (sdist, wheel))
+        sdist = f'dist/pychecker-{meta["__version__"]}.tar.gz'
+        wheel = f'dist/pychecker-{meta["__version__"]}-py2.py3-none-any.whl'
+        rc = os.system(f'twine upload "{sdist}" "{wheel}"')
         sys.exit(rc)
+
+
+class RunTests(test):
+    """
+    Run the unit tests
+    """
+
+    @staticmethod
+    def run_tests():
+        from unittest import TestLoader, TextTestRunner
+        tests_dir = pjoin(dirname(__file__), 'tests')
+        suite = TestLoader().discover(tests_dir)
+        result = TextTestRunner().run(suite)
+        sys.exit(0 if result.wasSuccessful() else -1)
